488272816f2890fa78b29f0f1b3580022bb30c30
Jin Wei <jinweiloh1998@gmail.com>
2018-09-27 20:55:34 +1000

    adjusted fireball

--- Assets/Scripts/AirPlaneScript.cs
+++ Assets/Scripts/AirPlaneScript.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class AirPlaneScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public PointLight pointLight;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // Update point light (sun)
+        pointLight.Update();
+
+        // Get renderer component (in order to pass params to shader)
+        MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
+
+        // Pass updated light positions to shader
+        renderer.material.SetColor("_PointLightColor", this.pointLight.color);
+        renderer.material.SetVector("_PointLightPosition", this.pointLight.GetWorldPosition());
+
+    }
 }

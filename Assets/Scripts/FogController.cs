e060d0b5d8df95798aa21faf604303f5e671a76c
Trent Branson <trent.branson@gmail.com>
2018-10-03 18:02:57 +1000

    Added prev commits that were rolled back

---
+++ Assets/Scripts/FogController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogController : MonoBehaviour {
+
+    public GameObject player;
+    public float viewMargin;
+
+    void Start()
+    {
+        // becuase the fireball is a prefab we will need to set the player
+        // ourselves
+        if (player == null)
+        {
+            player = GameObject.Find("Player");
+        }
+    }
+
+    void Update()
+    {
+        // if no longer in view (with some margin to be safe) destroy self
+        if (player.transform.position.z >
+            this.transform.position.z + viewMargin)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GenerateFog.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateFog : MonoBehaviour
+{
+
+    // the fireball prefab
+    public GameObject fog;
+    public GameObject player;
+    public float rowDistanceApart;
+    public float y;
+    public float rowsShowing;
+
+    float nextRowZ = 10;
+
+    void Start()
+    {
+        // gen the number of rows that should be showing
+        for (int i = 0; i < rowsShowing; i++)
+        {
+            GenNextRow();
+        }
+    }
+
+    private void Update()
+    {
+        // generate a new row if we have moved rowDistanceApart since generating
+        // the last one
+        if (player.transform.position.z + rowDistanceApart * rowsShowing
+                > nextRowZ)
+        {
+            GenNextRow();
+        }
+    }
+
+    void GenNextRow()
+    {
+        Instantiate(fog,
+                    new Vector3(0, y, nextRowZ),
+                    Quaternion.identity);
+        nextRowZ += rowDistanceApart;
+    }
+}
